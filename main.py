5cc1b3a31fb46112ce9dee1143475efbb0b12c26
liuxiaofang <1542147783@qq.com>
2023-01-04 01:00:25 +0800

    添加val_loss和val_accruacy

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
         plt.imshow(train_images[i])
         plt.xlabel(class_names[train_labels[i][0]])
     plt.show()
-
-    model = models.Sequential
+    # macos Sequential会报错, Sequential()才不会
+    model = models.Sequential()
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
@@ -37,8 +37,10 @@ if __name__ == '__main__':
 
     plt.plot(history.history['accuracy'], label='accuracy')
     plt.plot(history.history['val_accuracy'], label='val_accuracy')
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
     plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
+    plt.ylabel('Loss/Accuracy')
     plt.ylim([0.5, 1])
     plt.legend(loc='lower right')
     plt.show()
